ec2861b5057c148002ddf04da8c36e7bfe8eba4a
AmbrozePhoebus <zoeykoolman85@gmail.com>
2024-05-06 12:29:45 +0200

    Create basis-AI.py

---
+++ basis-AI.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+
+client = OpenAI("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")
+
+def roep_openai_aan(temp, tekst, taal):
+    maximaal_tokens = 1000
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Je bent een taaldeskundige"},
+            {"role": "user", "content": "Vertaal de tekts: " + tekst + " naar het " + taal}
+        ],
+    max_tokens=maximaal_tokens,
+    temperature=temp
+    )
+
+    return completion.choices[0].message.content.strip()
+
+vertaal = input("Wat wil je vertalen? ")
+
+welkeTaal = input("Naar welke taal? ")
+
+vertaling = roep_openai_aan(0.0, vertaal, welkeTaal)
+
+print (vertaling)
